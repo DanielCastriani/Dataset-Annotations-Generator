8dabd9086dbd367b924bf56ecf5d01fec549f6a3
DanielCastriani <dcastriani@gmail.com>
2018-09-05 10:55:46 -0300

    verifica arquivos

--- class_counter.py
+++ class_counter.py
@@ -1,5 +1,6 @@
 import xml.etree.ElementTree as ET
 import os
+import sys
 
 def find(p,cl,qtd):
 
@@ -37,6 +38,44 @@ def count():
     s += '\nTotal de arquivos:' + str(qtd_arqs)
     return cl,qtd,s
 
+def verifica_xml_img():
+    """
+        n = numero de arquivos
+        log = paths
+    """
+    log = ''
+    n = 0
+    ann_dir = 'annotations'     
+    img_dir = 'images'  
+
+    salva = False
+
+    for _, f_xml in enumerate(os.scandir(ann_dir)):
+        n +=1
+        verifica = os.path.join(img_dir,f_xml.name.replace('.xml','.png'))
+        if not os.path.exists(verifica):
+            log += f_xml.path + '\n'
+            log += verifica + '\n'
+            log += '-----------------------------------------------'
+            salva = True
+    if salva:
+        with open('log','w') as log_file:
+            log_file.write(log)
+
+    return n,log
+
 if __name__ == '__main__':
-    _,_,s = count()
+    s = ''
+    
+    
+    if len(sys.argv) == 1:
+        print('1 - Contar Classes')
+        print('2 - Verifica xml x imagem')
+    elif len(sys.argv) == 2:
+        if sys.argv[1] == '1':
+            _,_,s = count()
+        elif sys.argv[1] == '2':
+            n,s = verifica_xml_img()
+            print('{} arquivos verificados'.format(n))
+    
     print(s)
